68a2077816247c22a5655ad28b20f44e2abc9dbe
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-07 11:03:30 +0100

    added more functions

--- src/skeleton.test.ts
+++ src/skeleton.test.ts
@@ -1,5 +1,28 @@
-import sum from "./skeleton";
+import  sum,  {returnLastNumberInArray,
+   returnFirstNumberInArray,
+   returnSmallerOfTwoNumbers,
+   findAndReturnMinAndMax,
+   reverseAString
+  } from "./skeleton";
 
 test("sum adds two numbers", () => {
   expect(sum(4, 3)).toBe(7);
+});
+
+test("returns first number from array", () => {
+  expect(returnFirstNumberInArray([1,25,54,3])).toBe(1);
+});
+
+test("returns last number from array", () => {
+  expect(returnLastNumberInArray([1,25,54,3])).toBe(3);
+});
+
+test("returns smaller of 2 numbers", () => {
+  expect(returnSmallerOfTwoNumbers(5,10)).toBe(5);
+});
+test("returns min and max from array", () => {
+  expect(findAndReturnMinAndMax([6,5,1,100])).toEqual({min:1,max:100});
+});
+test("reverses a string", () => {
+  expect(reverseAString("tomasz")).toBe("zsamot");
 });
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -9,3 +9,64 @@ function sum(a: number, b: number): number {
 }
 
 export default sum;
+
+export function returnFirstNumberInArray(arrayToProcess:number[]|undefined):number|undefined{
+
+  let firstItemInArray:number|undefined
+  if(arrayToProcess!==undefined ) {
+    firstItemInArray = arrayToProcess.shift() 
+  }
+  return firstItemInArray;
+}
+
+export function returnLastNumberInArray(arrayToProcess:number[]|undefined):number|undefined{
+  let lastItemInArray:number|undefined
+  if(arrayToProcess !== undefined){
+    lastItemInArray  = arrayToProcess.pop();
+  }
+  return lastItemInArray;
+}
+
+export function returnSmallerOfTwoNumbers(num1:number,num2:number):number{
+  const smaller:number = num1<num2?num1:num2
+  
+  return smaller
+}
+
+interface MinAndMax {
+  min: number;
+  max: number;
+}
+
+export function findAndReturnMinAndMax(arrayToCheck:number[]):MinAndMax{
+  let min:number = arrayToCheck[0];
+  let max:number = arrayToCheck[0];
+
+  arrayToCheck.map((el) => 
+    {
+      if(el<min){
+        min = el
+      }
+    }
+  )
+
+  arrayToCheck.map((el) => 
+    {
+      if(el>max){
+        max = el
+      }
+    }
+    
+  )
+
+  const minAndMaxArray:MinAndMax = {
+    min : min,
+    max : max
+  }
+  return minAndMaxArray;
+}
+
+export function reverseAString(textToReverse:string):string{
+  const reversedStringAsArray:string[]= textToReverse.split("").reverse();
+  return reversedStringAsArray.toString().replace(/[,]/g,"");
+}
